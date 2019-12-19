083e71e470323adf6bdf2bd730361c8763bca21a
jdromero88 <github email address>
2019-12-19 12:44:33 -0500

    can create users

--- Back-End/sports-buddy-api/app/controllers/users_controller.rb
+++ Back-End/sports-buddy-api/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users.to_json(serialized_data)
+  end
+
+  def create
+    user = User.create(strong_user_params)
+    byebug
+    render json: user.to_json(serialized_data)
+  end
+
+  private
+
+  def strong_user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :location, :password_digest)
+  end
+
+  def serialized_data
+    {:except => [:created_at, :updated_at]}
+  end
+
 end
